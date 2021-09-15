e24daa47ae6445ff5dc1c8e1aec629a47335600b
grace.kiarie <grace.kiarie@qhala.com>
2021-09-15 23:02:50 +0300

    Style: refactor and add css

--- lib/fly_web/components/header_breadcrumbs.ex
+++ lib/fly_web/components/header_breadcrumbs.ex
@@ -17,7 +17,7 @@ defmodule FlyWeb.Components.HeaderBreadcrumbs do
                 </svg>
               <% end %>
 
-              <%= link label, to: href, class: "ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" %>
+              <%= link label, to: href, class: "ml-4 text-sm font-medium text-primary hover:text-gray-700" %>
             </div>
           </li>
         <% end %>
